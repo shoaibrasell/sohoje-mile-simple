1ca54b1ac8cb9bbc4a4edef281d297ea2cd9bacb
rasel <shoaibullahrasel@gmail.com>
2023-12-10 03:40:13 +0600

    added cart

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
+import Cart from "../Cart/Cart";
 
 const Shop = () => {
   const [products, setProduct] = useState([]);
@@ -14,7 +15,6 @@ const Shop = () => {
   }, []);
 
   const handleAddToCart = (product) => {
-
     //cart.push(product);
     const newCart = [...cart, product];
     setCart(newCart);
@@ -32,8 +32,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h4>Order Summary</h4>
-        <p>Selected items: {cart.length}</p>
+       <Cart cart={cart}></Cart>
       </div>
     </div>
   );
